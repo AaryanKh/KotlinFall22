2c4bea05b91fe163fba3fff9d92f57dd56fd685f
Aaryan Khan <111892632+AaryanKh@users.noreply.github.com>
2022-09-01 09:41:55 -0500

    Create 22Snippet.kt

---
+++ week02/22Snippet.kt
@@ -0,0 +1,19 @@
+fun main() {
+    var lucky: Char = '8'
+    println(lucky)
+}
+
+fun main(){
+    var eachDay: Double = 1.0
+    println(eachDay)
+}
+
+fun main(){
+    var age: int = 88
+    println(age)
+}
+
+fun main(){
+    var kotlinRocks: Boolean = false
+    println(kotlinRocks)
+}
