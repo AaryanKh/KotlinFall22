b99f36c84e9d5df18f054da65bee549cff96de97
Aaryan Khan <111892632+AaryanKh@users.noreply.github.com>
2022-09-06 10:20:52 -0500

    Create SalesPrediction.kt

---
+++ week02/SalesPrediction.kt
@@ -0,0 +1,6 @@
+fun main(){
+    var percentTotalSales = .62
+    var annualSales = 4.6
+    var totalSales = annualSales*percentTotalSales
+    println("The East Coast sales division is predicted to generate $totalSales million ")
+}
