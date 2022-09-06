07008f97e6ed2ac913f24362041444909c283e40
Aaryan Khan <111892632+AaryanKh@users.noreply.github.com>
2022-09-06 10:13:31 -0500

    Update StarPattern.kt

--- week02/StarPattern.kt
+++ week02/StarPattern.kt
@@ -1,9 +1,9 @@
 fun main(){
-	  println("   *")
-    println("  ***")
-    println(" *****")
-    println("*******")
-    println(" *****")
-	  println("  ***")
-    println("   *")
+	println("   *")
+    	println("  ***")
+    	println(" *****")
+    	println("*******")
+    	println(" *****")
+	println("  ***")
+    	println("   *")
 }
