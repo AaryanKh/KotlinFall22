f43b056e7d4be0c69dab70099cfa3ead6bc1a0ce
Aaryan Khan <111892632+AaryanKh@users.noreply.github.com>
2022-09-06 10:06:29 -0500

    Create nameageincome.kt

---
+++ week02/nameageincome.kt
@@ -0,0 +1,6 @@
+fun main(){
+    var name: String = "Aaryan Khan"
+    var age: Int = 23
+	var annualPay: Double = 90000.00
+    println("My name is $name, my age is $age and I hope to earn $$annualPay per year.")
+}
